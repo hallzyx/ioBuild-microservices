e32adce9954d46f7d1bf4e39feb443a2f7588ece
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2026-07-01 17:15:15 -0500

    feat(infra): expose Jaeger UI at watcher.arroz.dev
    - dns.tf: add cloudflare A record watcher.arroz.dev -> VM IP (proxied)
    - docker-compose.prod.yml: add jaeger service + OTEL_EXPORTER_OTLP_ENDPOINT
      to all microservices so traces are collected in production
    - nginx.conf: add server block for watcher.arroz.dev -> proxy jaeger:16686

--- infra/dns.tf
+++ infra/dns.tf
@@ -8,5 +8,14 @@ resource "cloudflare_record" "demo" {
   type    = "A"
   value   = azurerm_public_ip.vm.ip_address
   proxied = true
-  ttl     = 1 # 1 = automatic; required to be 1 when proxied
+  ttl     = 1
+}
+
+resource "cloudflare_record" "watcher" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "watcher"
+  type    = "A"
+  value   = azurerm_public_ip.vm.ip_address
+  proxied = true
+  ttl     = 1
 }
